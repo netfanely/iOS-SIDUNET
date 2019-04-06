b127a2b363c86dbb3486893f7cd3ac3c82336c89
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-04-06 14:06:40 -0500

    Add files via upload

---
+++ iOS-SIDUNET/Controller/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  iOS-SIDUNET
+//
+//  Created by Fanely on 4/6/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    //MARK: - Inicialización
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - CICLO DE VIDA
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
