f8b1578a98bb65c9850158687c7e3b67813c4971
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-04-06 14:06:11 -0500

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  iOS-SIDUNET
+//
+//  Created by Fanely on 4/6/19.
+//  Copyright © 2019 Net Fanely. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Setup
+        setupView()
+        
+        return true
+    }
+    // MARK: - Métodos Privados
+    
+    private func setupView() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+
+}
+
